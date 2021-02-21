38ae9093d27102498d1ea3f651415de1e1e5f913
Chidi Williams <williamschidi1@gmail.com>
2021-02-21 18:32:47 +0000

    Remove unneeded iife

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
 import simple from './simple';
 
-(() => {
-  const inputElement = document.querySelector('#input');
-  const outputElement = document.querySelector('#output');
+const inputElement = document.querySelector('#input');
+const outputElement = document.querySelector('#output');
 
-  inputElement.addEventListener('input', (event) => {
-    try {
-      const expression = event.target.value;
-      const result = simple.evaluate(expression);
-      outputElement.innerText = result ?? '';
-      outputElement.classList.remove('error');
-    } catch (error) {
-      outputElement.innerText = error;
-      outputElement.classList.add('error');
-    }
-  });
-})();
+inputElement.addEventListener('input', (event) => {
+  try {
+    const expression = event.target.value;
+    const result = simple.evaluate(expression);
+    outputElement.innerText = result ?? '';
+    outputElement.classList.remove('error');
+  } catch (error) {
+    outputElement.innerText = error;
+    outputElement.classList.add('error');
+  }
+});
