9542192765e061be3f7f909ff998ea75cfa3c0bc
Chidi Williams <williamschidi1@gmail.com>
2021-02-21 18:26:31 +0000

    Add homepage

---
+++ index.js
@@ -0,0 +1,18 @@
+import simple from './simple';
+
+(() => {
+  const inputElement = document.querySelector('#input');
+  const outputElement = document.querySelector('#output');
+
+  inputElement.addEventListener('input', (event) => {
+    try {
+      const expression = event.target.value;
+      const result = simple.evaluate(expression);
+      outputElement.innerText = result ?? '';
+      outputElement.classList.remove('error');
+    } catch (error) {
+      outputElement.innerText = error;
+      outputElement.classList.add('error');
+    }
+  });
+})();
