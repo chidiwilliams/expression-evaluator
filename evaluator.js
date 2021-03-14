bba4c5ffd8fda11583bf24f7ab8cd6aa4cc7ed3c
Chidi Williams <williamschidi1@gmail.com>
2021-03-14 13:59:13 +0000

    Add to RPN comments

--- evaluator.js
+++ evaluator.js
@@ -50,66 +50,97 @@ function tokenize(input) {
 
   return tokens;
 }
-
+/**
+ * Converts the tokens in infix notation to Reverse Polish notation
+ */
 function toRPN(tokens) {
+  // First, we'll set up a stack to hold operators we're not yet ready to
+  // add to the final expression
   const operators = [];
+
+  // ...and an array to hold the final RPN expression
   const out = [];
 
+  // For each token in the infix expression...
   for (let i = 0; i < tokens.length; i++) {
     const token = tokens[i];
 
-    if (typeof token === 'number') {
+    // If the token is a number...
+    if (typeof token === 'number' || /^[$#]/.test(token)) {
+      // We'll push it to `out`
       out.push(token);
       continue;
     }
 
+    // If the token is an operator...
     if (/[+\-/*<>=^]/.test(token)) {
+      // While there are operators in the `operators` stack with a higher
+      // precedence than the current token, we'll unwind them `operators` on to `out`
       while (shouldUnwindOperatorStack(operators, token)) {
         out.push(operators.pop());
       }
+
+      // And then, push the current token to the `operators` stack
       operators.push(token);
       continue;
     }
 
+    // If the token is a left parenthesis symbol...
     if (token === '(') {
+      // We'll push it to the `operators` stack
       operators.push(token);
       continue;
     }
 
+    // If the token is a right parenthesis symbol...
     if (token === ')') {
+      // While there are operators in the `operators` stack, and we haven't reached the
+      // last left parenthesis symbol, we'll unwind them on to `out`
       while (operators.length > 0 && operators[operators.length - 1] !== '(') {
         out.push(operators.pop());
       }
+
+      // We no longer need the left parenthesis symbol, so we'll discard it
       operators.pop();
       continue;
     }
 
+    // If the token is a comma...
     if (token === ',') {
+      // While there are operators in the `operators` stack, and we haven't reached the
+      // last left parenthesis symbol, we'll unwind them on to `out`
       while (operators.length > 0 && operators[operators.length - 1] !== '(') {
         out.push(operators.pop());
       }
       continue;
     }
 
-    if (/[A-Z$]/.test(token)) {
+    // If the token is a function name...
+    if (/[A-Z]/.test(token)) {
+      // We'll push it to the `operators` stack
       operators.push(token);
       continue;
     }
 
+    // If we can't recognize the token, we'll throw an error
     throw new Error(`Unparsed token ${token} at position ${i}`);
   }
 
+  // Finally we'll unwind all the remainin operators on to `out`
   for (let i = operators.length - 1; i >= 0; i--) {
     out.push(operators[i]);
   }
 
+  // And then return `out`
   return out;
 }
 
 // Multiplication and division have higher precedence than addition and
-// subtraction, à la BODMAS/PEMDAS.
+// subtraction. We know this from BODMAS, PEMDAS, etc.
 const precedence = { '*': 2, '/': 2, '+': 1, '-': 1 };
 
+// Returns true if the topmost operator in the `operators` stack should be
+// has a higher pre
 function shouldUnwindOperatorStack(operators, nextToken) {
   if (operators.length === 0) {
     return false;
@@ -163,8 +194,9 @@ function evalRPN(rpn) {
 }
 
 /**
- * Operates on the stack. The operator here is either an arithmetic or a
- * logical operator, so we only need two operands from the stack to operate on.
+ * Returns the result of appyling the mathematical operator on the stack.
+ * The operator here is either an arithmetic or a logical operator, so we
+ * only need two operands from the stack for any operator.
  */
 function operate(operator, stack) {
   const a = stack.pop();
@@ -193,20 +225,20 @@ function operate(operator, stack) {
 }
 
 /**
- * Applies the function onto the stack.
+ * Returns the result of applying the function onto the stack.
  *
  * Functions may have any number of arguments. But each function must have a
  * definite number of arguments. i.e. X(a, b) and Y(a, b, c) are possible, but
  * X(a, b) and X(a, b, c) are not possible.
  *
- * The function arguments are in right-to-left order in the stack. The rightmost
+ * The function arguments are in right-to-left order in the stack, i.e. the rightmost
  * argument is at the top of the stack, and so on.
  */
 function apply(func, stack) {
   // MAX(a, b) returns the larger of a and b
   if (func === 'MAX') {
-    const a = stack.pop();
     const b = stack.pop();
+    const a = stack.pop();
     return Math.max(a, b);
   }
 
@@ -239,6 +271,7 @@ function apply(func, stack) {
 /**
  * Finally, in the `evaluator` function, we'll link all stages
  * of the evaluation together.
+ *
  * tokenize -> toRPN -> evalRPN
  */
 function evaluate(input) {
